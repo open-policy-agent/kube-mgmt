3784576a8750dea283562369033c3b95e9d44450
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2022-02-05 00:37:43 +0200

    chore: lint tests
    Signed-off-by: Ievgenii Shepeliuk <eshepelyuk@gmail.com>

--- test/linter/test.sh
+++ test/linter/test.sh
@@ -5,6 +5,36 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+helm lint charts/opa --strict --set mgmt.enabled=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+helm lint charts/opa --strict --set sar.enabled=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+helm lint charts/opa --strict --set certManager.enabled=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+helm lint charts/opa --strict --set prometheus.enabled=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+helm lint charts/opa --strict --set admissionController.enabled=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+helm lint charts/opa --strict --set authz.enabled=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 echo "=================================================================================="
 echo "                                LINT PASSED"
 echo "=================================================================================="
