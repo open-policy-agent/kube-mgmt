07cba747e033f84274ee4bf7f06982893a3dd92d
Amente Bekele <amente.bekele@okta.com>
2022-11-15 21:47:24 -0500

    Implemented optional use of existing secret for mgmt token
    Signed-off-by: Amente Bekele <amente.bekele@okta.com>

---
+++ test/e2e/custom_mgmt_token/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+set -x
+
+TOKEN="mgmt-token-secret-value"
+OPA="http --ignore-stdin --default-scheme=https --verify=no -A bearer -a ${TOKEN} :8443/v1"
+
+${OPA}/data | jq -e '.result.test_helm_kubernetes_quickstart|keys|length==3'
+
+kubectl apply -f "$(dirname $0)/../fixture.yaml"
+
+${OPA}/policies | jq -e '.result|any(.id=="default/policy-include/include.rego")==true'
+${OPA}/data/example/include/allow | jq -e '.result==true'
+
+${OPA}/data/default | jq -e '.result|keys==["data-include"]'
+${OPA}/data/default/data-include | jq -e '.result["include.json"].inKey=="inValue"'
+
+kubectl get cm -l openpolicyagent.org/policy=rego -ojson | \
+  jq -e '.items[].metadata.annotations["openpolicyagent.org/kube-mgmt-status"]|fromjson|.status=="ok"'
+
+kubectl get cm -l openpolicyagent.org/data=opa -ojson | \
+  jq -e '.items[].metadata.annotations["openpolicyagent.org/kube-mgmt-status"]|fromjson|.status=="ok"'
