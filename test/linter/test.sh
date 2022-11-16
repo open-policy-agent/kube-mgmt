d0fb97918b5dff9b3cabcfa06fc60e7fce3a6354
Amente Bekele <amente.bekele@okta.com>
2022-11-15 21:47:24 -0500

    Added extraVolumeMounts for mounting extraVolumes
    Signed-off-by: Amente Bekele <amente.bekele@okta.com>

--- test/linter/test.sh
+++ test/linter/test.sh
@@ -50,6 +50,11 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+helm lint charts/opa-kube-mgmt --strict --set extraVolumeMounts\[0\].name=storage --set extraVolumeMounts\[0\].mountPath=/storage
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 echo "=================================================================================="
 echo "                                LINT PASSED"
 echo "=================================================================================="
