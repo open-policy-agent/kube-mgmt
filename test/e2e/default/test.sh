56d26a025c9c3b76795864eacd2fd71e27dcd380
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2022-12-30 17:41:08 +0000

    fix!: default config
    Signed-off-by: Ievgenii Shepeliuk <eshepelyuk@gmail.com>

--- test/e2e/default/test.sh
+++ test/e2e/default/test.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-set -e
-set -x
+#!/usr/bin/env sh
+set -ex
 
 TOKEN=$(kubectl exec deploy/kube-mgmt-opa-kube-mgmt -c mgmt -- cat /bootstrap/mgmt-token)
 OPA="http --ignore-stdin --default-scheme=https --verify=no -A bearer -a ${TOKEN} :8443/v1"
 
-${OPA}/data | jq -e '.result.test_helm_kubernetes_quickstart|keys|length==3'
+${OPA}/data | jq -e '.result|keys|length==0'
 
 kubectl apply -f "$(dirname $0)/../fixture.yaml"
 
