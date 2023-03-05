483e11cdd81518fe6ffc2c50ad78db2a4aa09577
Timothy Ehlers <tehlers@expedia.com>
2023-03-05 14:55:53 -0600

    review feedback
    Signed-off-by: Timothy Ehlers <tehlers@expedia.com>

---
+++ test/e2e/replication_ignore_namespaces/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -ex
+
+TOKEN=$(kubectl exec deploy/kube-mgmt-opa-kube-mgmt -c mgmt -- cat /bootstrap/mgmt-token)
+OPA="http --ignore-stdin --default-scheme=https --verify=no -A bearer -a ${TOKEN} :8443/v1"
+
+kubectl apply -f "$(dirname $0)/../fixture-replication.yaml"
+
+${OPA}/data/kubernetes/configmaps/ignore-me  | jq -e '.result|length==0'
+
+${OPA}/data/kubernetes/configmaps/dont-ignore-me  | jq -e '.result|length>=0'
