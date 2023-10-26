3af5708493ad8815d9b98f7073a7a41d5841daf7
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2023-10-26 09:40:23 +0000

    chore(e2e): improve test cases
    Signed-off-by: Ievgenii Shepeliuk <eshepelyuk@gmail.com>

--- test/e2e/multi/test.sh
+++ test/e2e/multi/test.sh
@@ -3,12 +3,13 @@ set -ex
 
 OPA="http --ignore-stdin :8080/v1"
 FX_OK="$(dirname $0)/../fixture-multi.yaml"
-FX_KO="$(dirname $0)/../fixture-multi-fail.yaml"
 
 ${OPA}/data/my_pkg/my_rule | jq -e '.result==null'
 
 kubectl apply -f ${FX_OK}
 sleep 3
+
+${OPA}/policies | jq -e '.result|length==2'
 
 ${OPA}/policies | jq -e '.result|any(.id=="default/multi-file-policy/a.rego")==true'
 ${OPA}/policies | jq -e '.result|any(.id=="default/multi-file-policy/b.rego")==true'
@@ -20,15 +21,6 @@ kubectl get cm multi-file-policy -ojson | \
 
 ${OPA}/data/my_pkg/my_rule input[hello]=world | jq -e '.result==true'
 ${OPA}/data/my_pkg/my_rule input[hello]=incorrect | jq -e '.result==false'
-
-######
-#
-######
-
-kubectl apply -f ${FX_KO}
-sleep 3
-
-${OPA}/policies | jq -e '.result|length==2'
 
 kubectl get cm multi-file-fail-policy -ojson | \
   jq -e '.metadata.annotations["openpolicyagent.org/kube-mgmt-status"]|fromjson|.status=="error"'
