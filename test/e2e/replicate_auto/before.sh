2563a91a9c10ddd5616ab05973d522305e0007cc
Torin Sandall <torin@styra.com>
2025-06-30 14:24:25 -0700

    test: run opa build from source
    requires a dependency on github.com/open-policy-agent/opa/cmd
    
    Signed-off-by: Torin Sandall <torin@styra.com>

--- test/e2e/replicate_auto/before.sh
+++ test/e2e/replicate_auto/before.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -ex
 
-opa build -b $(dirname $0)/bundle -o $(dirname $0)/bundle.tar.gz
+go run github.com/open-policy-agent/opa build -b $(dirname $0)/bundle -o $(dirname $0)/bundle.tar.gz
 kubectl delete configmap bundle || true
 kubectl create configmap bundle --from-file $(dirname $0)/bundle.tar.gz
